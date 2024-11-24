504372fb1e917bc99913f0ba9595f7e19f69c022
blejsi <148754773+blejsi@users.noreply.github.com>
2024-11-24 13:33:06 -0930

    projecti

--- src/main/java/com/example/Rent_a_car/controller/CarController.java
+++ src/main/java/com/example/Rent_a_car/controller/CarController.java
@@ -4,52 +4,40 @@ import com.example.Rent_a_car.DTO.CarRequestDto;
 import com.example.Rent_a_car.DTO.CarResponseDto;
 import com.example.Rent_a_car.service.CarService;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping("/api/rent")
 @RestController
+@RequestMapping("/api/rent")
 @AllArgsConstructor
-@NoArgsConstructor
 public class CarController {
 
-    private CarService carService;
+    private final CarService carService;
 
+    @GetMapping("/get")
+    public List<CarResponseDto> findAll() {
+        return carService.findAll();
+    }
 
-    @GetMapping
-public List<CarResponseDto> findAll(){
-    return carService.findAll();
-}
-
-
- @GetMapping
-    public CarResponseDto findById(@RequestBody Long id){
+    @GetMapping("/getById/{id}")
+    public CarResponseDto findById(@PathVariable Long id) {
         return carService.findById(id);
     }
 
-
-       @PostMapping
-    public CarResponseDto save(@RequestBody CarRequestDto carRequestDto){
+    @PostMapping("/save")
+    public CarResponseDto save(@RequestBody CarRequestDto carRequestDto) {
         return carService.save(carRequestDto);
     }
 
-
-
-@PutMapping
-public CarResponseDto update(@RequestBody Long id, CarRequestDto carRequestDto){
+    @PutMapping("/update/{id}")
+    public CarResponseDto update(@PathVariable Long id, @RequestBody CarRequestDto carRequestDto) {
         return carService.update(id, carRequestDto);
-}
-
-
-
-@GetMapping
-    public String delete(@RequestBody Long id){
-        return carService.delete(id);
     }
 
-
-
+    @DeleteMapping("/delete/{id}")
+    public String delete(@PathVariable Long id) {
+        return carService.delete(id);
+    }
 }
+
